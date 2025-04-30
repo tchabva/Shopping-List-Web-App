e54d16d47b77e227a37fb2a1fe1588f705bddf90
Tawanda Chabva <tchabva@gmail.com>
2025-04-30 18:42:32 +0100

    first commit

---
+++ src/main/java/uk/project/shoppinglistwebapp/ShoppinglistwebappApplication.java
@@ -0,0 +1,13 @@
+package uk.project.shoppinglistwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinglistwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/project/shoppinglistwebapp/ShoppinglistwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.project.shoppinglistwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
