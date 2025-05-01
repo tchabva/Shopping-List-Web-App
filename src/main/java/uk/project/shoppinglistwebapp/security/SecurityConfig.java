4b916a638f258143692c9b630c9d3989f9ab8317
Tawanda Chabva <tchabva@gmail.com>
2025-05-01 15:46:19 +0100

    fix: allow display of H2-Console for testing purporses

--- src/main/java/uk/project/shoppinglistwebapp/security/SecurityConfig.java
+++ src/main/java/uk/project/shoppinglistwebapp/security/SecurityConfig.java
@@ -4,6 +4,11 @@ import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +16,24 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     private static final String LOGIN_URL = "/login";
 
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/h2-console/**")
+                .permitAll()
+        ).headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .oauth2Login(withDefaults());
+
+        httpSecurity.csrf(csrf ->
+                csrf.ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**")
+                ).csrfTokenRepository(
+                        CookieCsrfTokenRepository.withHttpOnlyFalse()
+                )
+        );
+
         super.configure(httpSecurity);
         // Ensures any unauthenticated attempt to access the URL in the Application will be redirected to the login page
         httpSecurity.oauth2Login(c -> c.loginPage(LOGIN_URL).permitAll());
