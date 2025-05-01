14e7d993b1f44d74edfcae1671324a5c0802f3d9
Tawanda Chabva <tchabva@gmail.com>
2025-05-01 15:26:44 +0100

    feat: create MainView with a logout button

---
+++ src/main/java/uk/project/shoppinglistwebapp/ui/MainView.java
@@ -0,0 +1,49 @@
+package uk.project.shoppinglistwebapp.ui;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServletRequest;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+@Route("")
+@PermitAll // Allows any authenticated user to view this page
+public class MainView extends VerticalLayout {
+
+    private static final String LOGOUT_SUCCESS_URL = "/";
+
+    public MainView() {
+        // Using the raw Spring Security API directly do access Google provided
+        // credentials and doing logout. Check the GitHub example for a better basis
+        // an actual application, where these details are refactored to a separate UserSession bean
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+
+        String givenName = principal.getAttribute("given_name");
+        String familyName = principal.getAttribute("family_name");
+        String email = principal.getAttribute("email");
+        String picture = principal.getAttribute("picture");
+
+        H2 header = new H2("Hello " + givenName + " " + familyName + " (" + email + ")");
+        Image image = new Image(picture, "User Image");
+
+        // Logout Button
+        Button logoutButton = new Button("Logout", click -> {
+            UI.getCurrent().getPage().setLocation(LOGOUT_SUCCESS_URL);
+            SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+            logoutHandler.logout(
+                    VaadinServletRequest.getCurrent().getHttpServletRequest(), null,
+                    null);
+        });
+
+        setAlignItems(Alignment.CENTER);
+        add(header, image, logoutButton);
+    }
+}
